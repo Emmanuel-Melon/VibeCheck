9d8a7fcaeda4783cdf3ea99e64e999a1b27661ea
Eman <emmanuelgatwech@gmail.com>
2023-12-24 23:30:48 +0300

    chore: init hasura

--- apps/native/App.js
+++ apps/native/App.js
@@ -1,5 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
 import { StyleSheet, Text, View } from 'react-native';
+
+// Initialize Apollo Client
+const client = new ApolloClient({
+  uri: 'http://localhost:4000/graphql',
+  cache: new InMemoryCache()
+});
 
 export default function App() {
   return (
